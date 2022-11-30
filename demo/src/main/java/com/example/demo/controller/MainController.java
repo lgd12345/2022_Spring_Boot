f1ff4d27bf2364eb5ba6051cbb9e8116ad2e982b
GD <lkd1126@naver.com>
2022-11-30 16:50:44 +0900

    2022.11.30리다이렉트개념

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -12,4 +12,11 @@ public class MainController {
 	public String index() {
 		return "index";
 	}
+//	/question/list URL페이지를 리다이렉트 하라는 명령어다.
+//	redirect:/URL : URL로 리다이렉트(리다이렉는 완전히 새로운 URL로의 요청이 됨)
+//	forward:/URL : URL로 포워드 (포워드는 기존 요청 값들이 유지된 상태로 URL전환됨)
+	@RequestMapping("/")
+	public String root() {
+		return "redirect:/question/list";
+	}
 }
